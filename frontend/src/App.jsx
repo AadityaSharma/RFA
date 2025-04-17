c1f277a659e379dda466f11677d11baa5abbd803
Aaditya Sharma <AadityaSharma@users.noreply.github.com>
2025-04-17 21:06:07 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
 // frontend/src/App.jsx
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 import Projects from './pages/Projects';
 import Entries from './pages/Entries';
 import Dashboard from './pages/Dashboard';
+import Admin from './pages/Admin';
 import ProtectedRoute from './components/ProtectedRoute';
+import Navbar from './components/Navbar';
 
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
-      <Route path="/projects" element={<ProtectedRoute><Projects/></ProtectedRoute>}/>
-      <Route path="/entries" element={<ProtectedRoute><Entries/></ProtectedRoute>}/>
-    </Routes>
-  );
-}
+ export default function App() {
+   return (
+    <>
+      <Navbar/>
+       <Routes>
+        <Route path="/signup" element={<Signup/>}/>
+         <Route path="/login" element={<Login/>}/>
+         <Route path="/" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
+         <Route path="/projects" element={<ProtectedRoute><Projects/></ProtectedRoute>}/>
+         <Route path="/entries" element={<ProtectedRoute><Entries/></ProtectedRoute>}/>
+        <Route path="/admin" element={<ProtectedRoute><Admin/></ProtectedRoute>}/>
+       </Routes>
+    </>
+   );
+ }
